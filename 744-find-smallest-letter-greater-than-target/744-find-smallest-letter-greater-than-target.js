cbf261e4f79724cdf8401f567488cc83c13ac771
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-01-07 12:58:42 +0530

    Time: 80 ms (65.41%), Space: 40.7 MB (62.19%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
@@ -6,9 +6,11 @@
 var nextGreatestLetter = function(letters, target) {
     var s=0
     var e=letters.length-1
+    var boundaryIndex = -1
     while(s<=e){
         var m=Math.floor((s+e)/2)
         if(letters[m]>target){
+            boundaryIndex = m
             e=m-1;
         }
         else{
