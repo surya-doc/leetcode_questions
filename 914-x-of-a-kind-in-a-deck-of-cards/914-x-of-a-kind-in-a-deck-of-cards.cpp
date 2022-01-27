e1bfe7a7a3c3e946955b2f538daa5d91b3c52519
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-01-28 00:37:20 +0530

    Time: 36 ms (9.00%), Space: 17.5 MB (20.90%) - LeetHub

---
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        map<int, int>ct;
+        int count;
+        if(deck.size() <= 1) return false;
+        for(auto i:deck){
+            ct[i]++;
+        }
+        count = ct[deck[0]];
+        for(auto it:ct){
+            cout << it.second;
+           count = gcd(count, it.second);
+        }
+        cout << count;
+        if(count == 1) return false;
+        return true;
+    }
+};
