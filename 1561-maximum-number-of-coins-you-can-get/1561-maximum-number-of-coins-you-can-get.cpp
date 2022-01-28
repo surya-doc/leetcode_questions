f0d9c54e37b29cedbd96c519f646b70ec204c4ea
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-01-28 11:36:47 +0530

    Time: 269 ms (5.05%), Space: 53.4 MB (94.61%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -4,9 +4,6 @@ public:
         sort(piles.begin(), piles.end(), greater<int>());
         int n = piles.size()/3;
         int ans = 0;
-        for(auto it:piles){
-            cout << it;
-        }
         for(int i=0; i<n; i++){
             ans += piles[(2*i)+1];
         }
