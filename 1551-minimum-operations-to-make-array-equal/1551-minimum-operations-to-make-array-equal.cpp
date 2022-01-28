87254588a627dad92b363127b47247e28352c977
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-01-28 09:37:01 +0530

    Time: 0 ms (100.00%), Space: 6 MB (33.97%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -3,7 +3,7 @@ public:
     int minOperations(int n) {
         int ans = 0;
         for(int i=0; i<n/2; i++){
-            ans += (n-((2*i)+1));
+            ans += (n-(2*i)-1);
         }
         return ans;
     }
