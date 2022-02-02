46701120b0b44bd65ef787ffda97ab757ba454ca
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-02 12:20:28 +0530

    Time: 99 ms (37.89%), Space: 41.7 MB (5.26%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.js
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.js
@@ -3,10 +3,22 @@
  * @return {boolean}
  */
 var hasAlternatingBits = function(n) {
+//     var last = n&1;
+//     n>>=1; 
+//     while(n>0){
+//         if(!(n&1)^last){
+//             return false;
+//         }
+//         last = n&1;
+//         n>>=1;
+
+//     }
+//     return true;
+// var alternate = true;
     var last = n&1;
     n>>=1; 
     while(n>0){
-        if(!(n&1)^last){
+        if(last===(n&1)){
             return false;
         }
         last = n&1;
