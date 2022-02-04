0075c409517f24e6b83b2d08c19a9292b68a54ed
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-04 16:08:31 +0530

    Time: 17 ms (36.73%), Space: 9.3 MB (53.33%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        int row = mat.size();
+        int col = mat[0].size();
+        // vector<vector<int>>ans;
+        for(int i=0; i<col; i++){
+            int start_row = 0;
+            int start_col = i;
+            vector<int>stor;
+            while(start_row<row && start_col<col){
+                // cout << start_row << "," << start_col << " ";
+                stor.push_back(mat[start_row][start_col]);
+                start_row++;
+                start_col++;
+            }
+            sort(stor.begin(), stor.end());
+            start_row = 0;
+            start_col = i;
+            int posi = 0;
+            while(start_row<row && start_col<col){
+                mat[start_row][start_col] = stor[posi++];
+                start_row++;
+                start_col++;
+            }
+            // for(auto it:stor){
+            //     cout << it << " ";
+            // }
+            cout << "\n";
+        }
+        for(int i=1; i<row; i++){
+            int start_row_1 = i;
+            int start_col_1 = 0;
+            vector<int>stor_1;
+            while(start_row_1<row && start_col_1<col){
+                // cout << start_row_1 << "," << start_col_1 << " ";
+                stor_1.push_back(mat[start_row_1][start_col_1]);
+                start_row_1++;
+                start_col_1++;
+            }
+            sort(stor_1.begin(), stor_1.end());
+            start_row_1 = i;
+            start_col_1 = 0;
+            int posi_1 = 0;
+            while(start_row_1<row && start_col_1<col){
+                mat[start_row_1][start_col_1] = stor_1[posi_1++];
+                start_row_1++;
+                start_col_1++;
+            }
+        }
+        return mat;
+    }
+};
