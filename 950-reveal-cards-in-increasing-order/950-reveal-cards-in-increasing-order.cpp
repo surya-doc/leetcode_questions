03f3e08eb055a39b85dec531ac32764c12f96e2e
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-06 00:44:48 +0530

    Time: 6 ms (46.27%), Space: 8.5 MB (28.26%) - LeetHub

---
+++ 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int siz = deck.size();
+        deque<int>dq;
+        vector<int>ans(siz);
+        vector<int>track;
+        sort(deck.begin(), deck.end());
+        for(int i=0; i<siz; i++){
+            dq.push_back(i);
+        }
+        int i = 0;
+        while(dq.size() > 0){
+            ans[dq.front()] = deck[i++];
+            dq.pop_front();
+            dq.push_back(dq.front());
+            dq.pop_front();
+        }
+        return ans;
+    }
+};
