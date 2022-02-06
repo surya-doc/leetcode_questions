3d0fa0f3c3edd6f9575fbda38949c8d885a32739
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-06 10:02:28 +0530

    Time: 375 ms (12.21%), Space: 57.1 MB (7.57%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int calCulatePower(int num, int steps, unordered_map<int, int> &mp){
+        if(num == 1) return steps;
+        if(num%2 != 0){
+            if(mp[(num*3)+1]) return mp[(num*3)+1]+1;
+            else steps = calCulatePower((num*3)+1, steps, mp)+1;
+        }
+        else{
+            if(mp[num/2]) return mp[num/2]+1;
+            else steps = calCulatePower(num/2, steps, mp)+1;
+        }
+        mp[num] = steps;
+        // cout << num << "->" << steps << " ";
+        return steps;
+    }
+    
+    static int sort_1(vector<int> &x, vector<int> &y){
+        if(x[0] != y[0]) return x[0] < y[0];
+        return x[1] < y[1];
+    }
+    
+    int getKth(int lo, int hi, int k) {
+        int ans = 1;
+        unordered_map<int, int>mp;
+        vector<vector<int>>stor;
+        for(int i=lo; i<=hi; i++){
+            int temp = calCulatePower(i, 0, mp);
+            stor.push_back({temp, i});
+        }
+        sort(stor.begin(), stor.end(), sort_1);
+        int i = 1;
+        for(auto it:stor){
+            if(i == k){
+                ans = it[1];
+                break;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
