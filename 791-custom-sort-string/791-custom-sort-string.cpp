e7f349bb171cece6d0794640ad93943f06c8832a
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-06 19:34:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (99.88%) - LeetHub

---
+++ 791-custom-sort-string/791-custom-sort-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string ans = "";
+        string rest = "";
+        int flag = 0;
+        for(int i=0; i<order.length(); i++){
+            int j = 0;
+            while(flag < s.length() && j < s.length()){
+                if(s[j] == order[i]){
+                    swap(s[flag], s[j]);
+                    flag++;
+                }
+                j++;
+            }        
+        }
+        return s;
+    }
+};
