cf1401942c2d7e5c9f9f9287ac25258379245df7
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-07 00:54:52 +0530

    Time: 726 ms (5.19%), Space: 112.7 MB (5.99%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -4,21 +4,6 @@ public:
         return a[1] > b[1];
     }
     int minSetSize(vector<int>& arr) {
-        // vector<int>stor;    
-        // unordered_map<int, int> mp;
-        // for(int i=0; i<arr.size(); i++){
-        //     mp[arr[i]]++;
-        // }
-        // for(auto it:mp){
-        //     stor.push_back(it.first);
-        // }
-        // sort(stor.begin(), stor.end());
-        // int size = 0;
-        // int i=0;
-        // while(size >= (arr.size()/2)){
-        //     size += mp[arr[i++]];
-        // }
-        // return i-1;
         int ans = 0;
         map<int, int>mp;
         vector<vector<int>> stor;
@@ -31,7 +16,6 @@ public:
         }
         sort(stor.begin(), stor.end(), getOrder);
         for(auto it:stor){
-            cout << it[0] <<"," << it[1] << "\n";
             temp += it[1];
             ans++;
             if(temp >= arr.size()/2) break;
