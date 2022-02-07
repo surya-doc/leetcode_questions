1e00b017bf3713873eed20a4e1abd3f376016be3
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-07 19:39:00 +0530

    Time: 27 ms (18.98%), Space: 8.3 MB (77.92%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
-    static bool orderNumber(pair<char, int> &a, pair<char, int> &b){
-        return a.second > b.second;
-    }
-    
     string frequencySort(string s) {
         sort(s.begin(), s.end());
         string ans = "";
         pair<char, int>stor[256] = {{0, 0}};
-        // int count = 0;
-        // int i = 0;
-        // for(; i<s.length()-1; i++){
-        //     count++;
-        //     if(s[i] != s[i+1]){
-        //         stor.push_back({s[i], count});
-        //         count = 0;
-        //     }
-        // }
-        // if(s[i] != s[i-1]){
-        //     stor.push_back({s[i], 1});
-        // }
-        // else{
-        //     stor.push_back({s[i], count+1});
-        // }
-        // sort(stor.begin(), stor.end(), orderNumber);
-        // string a;
-        // for(auto it:stor){
-        //     a.append(it.second, it.first);
-        // }
-        // return a;
         for(auto it:s){
             stor[it].first = it;
             ++stor[it].second;
         }
-        sort(begin(stor), end(stor), orderNumber);
+        sort(begin(stor), end(stor), [](pair<char, int>& l, pair<char, int>& r) {
+            return (l.second > r.second);});
         for(auto it:stor){
             ans.append(it.second, it.first);
         }
