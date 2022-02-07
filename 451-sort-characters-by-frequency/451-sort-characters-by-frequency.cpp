5aeb0730ddea5372a96566fc9579179ffe26ee9e
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-07 19:36:29 +0530

    Time: 34 ms (12.70%), Space: 8.4 MB (77.92%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -6,27 +6,37 @@ public:
     
     string frequencySort(string s) {
         sort(s.begin(), s.end());
-        vector<pair<char, int>>stor;
-        int count = 0;
-        int i = 0;
-        for(; i<s.length()-1; i++){
-            count++;
-            if(s[i] != s[i+1]){
-                stor.push_back({s[i], count});
-                count = 0;
-            }
+        string ans = "";
+        pair<char, int>stor[256] = {{0, 0}};
+        // int count = 0;
+        // int i = 0;
+        // for(; i<s.length()-1; i++){
+        //     count++;
+        //     if(s[i] != s[i+1]){
+        //         stor.push_back({s[i], count});
+        //         count = 0;
+        //     }
+        // }
+        // if(s[i] != s[i-1]){
+        //     stor.push_back({s[i], 1});
+        // }
+        // else{
+        //     stor.push_back({s[i], count+1});
+        // }
+        // sort(stor.begin(), stor.end(), orderNumber);
+        // string a;
+        // for(auto it:stor){
+        //     a.append(it.second, it.first);
+        // }
+        // return a;
+        for(auto it:s){
+            stor[it].first = it;
+            ++stor[it].second;
         }
-        if(s[i] != s[i-1]){
-            stor.push_back({s[i], 1});
-        }
-        else{
-            stor.push_back({s[i], count+1});
-        }
-        sort(stor.begin(), stor.end(), orderNumber);
-        string a;
+        sort(begin(stor), end(stor), orderNumber);
         for(auto it:stor){
-            a.append(it.second, it.first);
+            ans.append(it.second, it.first);
         }
-        return a;
+        return ans;
     }
 };
