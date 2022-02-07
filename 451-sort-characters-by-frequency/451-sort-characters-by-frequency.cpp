eb9d3c009cfdbe5e46fddffa71e013cf8f235df6
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-07 18:54:18 +0530

    Time: 17 ms (44.62%), Space: 8.6 MB (43.33%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    static bool orderNumber(pair<char, int> &a, pair<char, int> &b){
+        return a.second > b.second;
+    }
+    
+    string frequencySort(string s) {
+        sort(s.begin(), s.end());
+        vector<pair<char, int>>stor;
+        int count = 0;
+        int i = 0;
+        for(; i<s.length()-1; i++){
+            count++;
+            if(s[i] != s[i+1]){
+                stor.push_back({s[i], count});
+                count = 0;
+            }
+        }
+        if(s[i] != s[i-1]){
+            stor.push_back({s[i], 1});
+        }
+        else{
+            stor.push_back({s[i], count+1});
+        }
+        for(auto it:stor){
+            cout << it.first << " " << it.second << "\n";
+        }
+        sort(stor.begin(), stor.end(), orderNumber);
+        string a;
+        for(auto it:stor){
+            a += string(it.second, it.first);
+        }
+        cout << a;
+        return a;
+    }
+};
