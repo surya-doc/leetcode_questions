60985c8b6a3fc58f559a396ad8a01c83c7ccd0a2
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-12 12:12:07 +0530

    Time: 736 ms (59.44%), Space: 105 MB (58.60%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -4,18 +4,21 @@ public:
         priority_queue<int>pq;
         int ans = 0;
         for(auto it:piles){
+            ans += it;
             pq.push(it);
         }
         for(int i=0; i<k; i++){
             int temp = pq.top();
             pq.pop();
+            ans -= temp;
             temp = (temp%2 == 0) ? temp/2 : (temp/2)+1;
             pq.push(temp);
+            ans += temp;
         }
-        while(pq.size() > 0){
-            ans += pq.top();
-            pq.pop();
-        }
+        // while(pq.size() > 0){
+        //     ans += pq.top();
+        //     pq.pop();
+        // }
         return ans;
     }
 };
