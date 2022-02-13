7bd8105460bb3759df0e6c57b4ea909df38133fd
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-13 16:07:58 +0530

    Time: 24 ms (5.76%), Space: 6.2 MB (42.47%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    bool check(int i, int j, string s){
-        set<char>st;
-        for(int a=j; a<=i; a++){
-            if(st.find(s[a]) != st.end()){
-                return false;
-            }
-            st.insert(s[a]);
-        }
-        return true;
-    }
+    // bool check(int i, int j, string s){
+    //     set<char>st;
+    //     for(int a=j; a<=i; a++){
+    //         if(st.find(s[a]) != st.end()){
+    //             return false;
+    //         }
+    //         st.insert(s[a]);
+    //     }
+    //     return true;
+    // }
     int countGoodSubstrings(string s) {
         int i = 2;
         int j = 0;
         int ans = 0;
-        for(; i<s.length(); i++){
-            if(check(i, j, s)) ans++;
-            j++;
+        for(; i<s.length(); i++,j++){
+            cout << j;
+            cout << s[j] << s[j+1] << s[i] << " ";
+            if(s[j] != s[j+1] && s[j] != s[i] && s[j+1] != s[i]) ans++;
         }
         return ans;
     }
