a039d271e7c00d4a5c95ae3e2e7bf080566723ad
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-17 23:07:27 +0530

    Time: 24 ms (75.48%), Space: 9.8 MB (99.44%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        map<char, int>mp;
-        mp['a'] = 1;
-        mp['e'] = 1;
-        mp['i'] = 1;
-        mp['o'] = 1;
-        mp['u'] = 1;
         int ans = 0;
         int tem_count = 0;
         int n = s.size();
         int i = 0, j = 0;
         for(; j<n; j++){
-            if(mp[s[j]]){
+            if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u'){
                 tem_count++;
             }
             if(j-i+1 == k){
                 ans = max(ans, tem_count);
-                if(mp[s[i]]){
+                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
                     tem_count--;
                 }
                 i++;
