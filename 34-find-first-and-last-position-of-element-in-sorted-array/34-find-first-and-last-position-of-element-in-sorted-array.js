d1d05f0b908d0ff545af9caff4a78dc7f1cd34a7
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-19 13:25:46 +0530

    Time: 415 ms (5.10%), Space: 53.4 MB (6.78%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function sendPos(nums, target, di, n){
+    var l = 0;
+    var r = n-1;
+    var returnable = -1;
+    while(l <= r){
+        var mid = l+(r-l)//2;
+        console.log(mid);
+        if(nums[mid] === target){
+            if(di){
+                returnable = mid;
+                r = mid-1;
+            }
+            else{
+                returnable = mid;
+                l = mid+1;
+            }
+        }
+        else if(nums[mid] >  target){
+            r = mid-1;
+        }
+        else{
+            l = mid+1;
+        }
+    }
+    console.log(returnable);
+    return returnable;
+}
+var searchRange = function(nums, target) {
+    var start = sendPos(nums, target, true, nums.length);
+    var end = sendPos(nums, target, false, nums.length);
+    var ans = [start, end];
+    return ans;
+};
