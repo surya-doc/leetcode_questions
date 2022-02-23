78da6cd97ebdccda70ec9500830ce5c774b9db66
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-23 21:01:43 +0530

    Time: 99 ms (16.80%), Space: 18.9 MB (15.42%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool check(vector<int> &piles, int mid, int h){
+        cout << mid << " ";
+        int n = piles.size();
+        int total_hr = 0;
+        for(int i=0; i<n; i++){
+            total_hr += (piles[i]%mid == 0) ? (piles[i]/mid) : ((piles[i]/mid)+1);
+        }
+        if(total_hr > h) return 0;
+        return 1;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(), piles.end());
+        int l = 1;
+        int r = piles[piles.size()-1];
+        int ans = r;
+        while(l <= r){
+            // cout << ans;
+            int mid = l+(r-l)/2;
+            int temp = check(piles, mid, h);
+            if(temp){
+                // cout << mid;
+                r = mid-1;
+                ans = min(ans, mid);
+                // cout << ans;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return ans;
+    }
+};
