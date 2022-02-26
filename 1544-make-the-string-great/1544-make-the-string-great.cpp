24adaa59b85423eb240d118f4d8971fed7414889
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-26 20:23:48 +0530

    Time: 4 ms (58.99%), Space: 6.6 MB (46.39%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     string makeGood(string s) {
+        // for(int i=0; i<s.length(); i++){
+        //     if(s[i]+32 == s[i+1] || s[i]-32 == s[i+1]){
+        //         return makeGood(s.substr(0, i)+s.substr(i+2));
+        //     }
+        // }
+        // return s;
+        stack<char>st;
+        string ans = "";
         for(int i=0; i<s.length(); i++){
-            if(s[i]+32 == s[i+1] || s[i]-32 == s[i+1]){
-                return makeGood(s.substr(0, i)+s.substr(i+2));
+            if(!st.empty() && (st.top()+32 == s[i] || st.top()-32 == s[i])){
+                cout << s[i];
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
             }
         }
-        return s;
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
