5b4fd25e788479a76f23c1af7f03681afe9be7c1
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-02-27 20:26:32 +0530

    Time: 11 ms (44.23%), Space: 9 MB (16.34%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int getTheNextGreater(stack<int>temp, int num){
-        int greater = -1;
-        while(temp.top() != num){
-            if(temp.top() > num) greater = temp.top();
-            temp.pop();
-        }
-        return greater;
-    }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int>st;
+        map<int, int>mp;
+        for(int i=0; i<nums1.size(); i++){
+            mp[nums1[i]] = -1;
+        }
         vector<int>ans(nums1.size());
-        for(int i=0; i<nums2.size(); i++){
+        for(int i=nums2.size()-1; i>=0; i--){
+            while(!st.empty() && (st.top() <= nums2[i])){
+                st.pop();
+            }
+            cout << nums2[i];
+            if(mp[nums2[i]]){
+                mp[nums2[i]] = st.empty() ? -1 : st.top();
+            }
             st.push(nums2[i]);
         }
         for(int i=0; i<nums1.size(); i++){
-            stack<int>temp = st;
-            ans[i] = getTheNextGreater(temp, nums1[i]);
+            ans[i] = mp[nums1[i]];
         }
         return ans;
     }
