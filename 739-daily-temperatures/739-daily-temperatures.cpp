19810abcbc62d1b8fdd709c3ea3bc64446a66990
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-03-01 17:53:58 +0530

    Time: 264 ms (38.61%), Space: 85.1 MB (71.95%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -8,17 +8,13 @@ public:
                 st.pop();
             }
             if(!st.empty()){
-                cout << st.top();
                 ans[i] = st.top()-i;
             }
             else{
                 ans[i] = 0;
-                cout << 0;
             }
             st.push(i);
         }
-        st.push(1);
-        cout << st.top();
         return ans;
     }
 };
