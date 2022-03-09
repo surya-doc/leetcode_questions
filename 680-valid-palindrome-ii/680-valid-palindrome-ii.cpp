c7da9243ebd3ea80395f349864e5899bf05efb9f
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-03-09 20:07:24 +0530

    Time: 64 ms (71.20%), Space: 24.1 MB (17.97%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    public:
+    bool isPalindrome(string s, int i, int j){
+        while(i<=j){
+            // cout << s[i] << s[j] << "\n";
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int count = 0;
+        int i = 0;
+        int j = s.size()-1;
+        while(i<=j){
+            if(s[i] != s[j]){
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
