4f7f7e7b87743ab96df2ed823a2f1cb2d7f66247
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-03-16 19:59:30 +0530

    Time: 66 ms (5.06%), Space: 6.8 MB (43.50%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        map<char, int>mp;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            mp[s[i]] = i;
+        }
+        vector<int>ans;
+        for(int i=0; i<n;){
+            int a = mp[s[i]];
+            int max_1 = a;
+            int prev = i;
+            for(int j=i; j<=a; j++){
+                if(mp[s[j]] > max_1){
+                    max_1 = mp[s[j]];
+                    a = max_1;
+                }
+                cout << max_1 << " ";
+            }
+        cout << "\n";
+            ans.push_back(max_1-prev+1);
+            prev = max_1;
+            i = max_1+1;
+        }
+        return ans;
+    }
+};
