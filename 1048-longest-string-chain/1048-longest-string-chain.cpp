1f01f0ed48105eafda770d927fa2cf3d778afa81
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-03-18 01:17:02 +0530

    Time: 210 ms (26.60%), Space: 50.7 MB (9.90%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -13,7 +13,7 @@ public:
             int max_temp = 0;
             for(int j=0; j<words[i].size(); j++){
                 string temp = words[i].substr(0, j)+words[i].substr(j+1);
-                mp[words[i]] = max(mp[words[i]], mp.find(temp) == mp.end() ? 1 : mp[temp]+1);
+                mp[words[i]] = max(mp[words[i]], mp[temp] == 0 ? 1 : mp[temp]+1);
             }
             ans = max(ans, mp[words[i]]);
         }
