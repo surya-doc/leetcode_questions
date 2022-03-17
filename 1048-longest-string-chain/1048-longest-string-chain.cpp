4a0185db80b852962ee00fc9dcecc5a79ee42fee
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-03-18 01:16:28 +0530

    Time: 101 ms (69.27%), Space: 16.6 MB (81.28%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -4,39 +4,12 @@ public:
         return s1.size() < s2.size();
     }
     
-    bool check(string s1, string s2){
-        if(s1.size()-s2.size() != 1){
-            return false;
-        }
-        int i = 0;
-        int j = 0;
-        int count = 0;
-        while(i < s1.size()){
-            
-            if(s1[i] != s2[j] && count == 0){
-                i++;
-                count++;
-            }
-            else if(s1[i] != s2[j] && count > 0){
-                return false;
-            }
-            else{
-                i++;
-                j++;
-            }
-            
-        }
-        return true;
-    }
-    
     int longestStrChain(vector<string>& words) {
         sort(words.begin(), words.end(), orderLen);
         int ans = 0;
-        // int j = words.size()-1;
         int temp = 0;
         unordered_map<string, int>mp;
         for(int i=0; i<words.size(); i++){
-            // mp[words[i]]++;
             int max_temp = 0;
             for(int j=0; j<words[i].size(); j++){
                 string temp = words[i].substr(0, j)+words[i].substr(j+1);
