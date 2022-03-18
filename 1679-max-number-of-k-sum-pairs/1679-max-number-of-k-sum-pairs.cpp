2b045190a7a1f9820f226d5e44f757c972d250a4
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-03-18 23:09:59 +0530

    Time: 236 ms (39.04%), Space: 67.6 MB (41.96%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int, int>mp;
         int ans = 0;
-//         for(int i=0; i<nums.size(); i++){
-//             mp[nums[i]]++;
-//         }
-//         for(auto it:mp){
-//             if(it.first < k){
-//                 if(k-it.first == (k/2) && k%2 == 0){
-//                     ans += (it.second)/2;
-//                 }
-//                 else{
-//                     if(mp[k-it.first] >= 1){
-//                         int l_temp = it.second;
-//                         int r_temp = mp[k-it.first];
-//                         int add = min(l_temp, r_temp);
-//                         ans += add;
-//                         mp[it.first] -= add;
-//                         mp[k-it.first] -= add;
-//                     }
-//                 }
-//             }
-
-//         }
+        unordered_map<int, int>mp;
         sort(nums.begin(), nums.end());
-        int i=0, j=nums.size()-1;
-        while(i<j){
-            if(nums[j]+nums[i] == k){
-                ans++;
-                j--;
-                i++;
+        // int i=0, j=nums.size()-1;
+        // while(i<j){
+        //     if(nums[j]+nums[i] == k){
+        //         ans++;
+        //         j--;
+        //         i++;
+        //     }
+        //     else{
+        //         if(nums[i]+nums[j] > k){
+        //             j--;
+        //         }
+        //         else{
+        //             i++;
+        //         }
+        //     }
+        // }
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto it:mp){
+            if(k-it.first == it.first){
+                ans += (it.second)/2;
             }
-            else{
-                if(nums[i]+nums[j] > k){
-                    j--;
-                }
-                else{
-                    i++;
-                }
+            else if(mp.count(k-it.first)){
+                int l_temp = it.second;
+                int r_temp = mp[k-it.first];
+                int add = min(l_temp, r_temp);
+                ans += add;
+                mp[it.first] -= add;
+                mp[k-it.first] -= add;
             }
         }
         return ans;
