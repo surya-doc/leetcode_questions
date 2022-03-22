69601612d22ff7883f38cc19b829290c4c184a26
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-03-22 22:13:24 +0530

    Time: 4 ms (43.97%), Space: 6 MB (49.72%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string makeAns(string ans, int k, vector<int> &org, int fact){
+        cout << fact;
+        if(org.size() == 0 || fact == 0) return ans;
+        ans += to_string(org[k/fact]);
+        // cout << ans;
+        org.erase(org.begin()+k/fact);
+        if(org.size() == 0) return ans;
+        k = k%fact;
+        cout << org.size();
+        fact = fact/org.size();
+        return makeAns(ans, k, org, fact);
+    }
+    
+    string getPermutation(int n, int k) {
+        // string org = "";
+        vector<int> org;
+        for(int i=0; i<n; i++){
+            org.push_back(i+1);
+        }
+        int fact = 1;
+        for(int i=1; i<n; i++){
+            fact *= i;
+        }
+        // cout << fact;
+        string ans = "";
+        ans = makeAns(ans, k-1, org, fact);
+        return ans;
+    }
+        
+};
