b2ac62680484678976f33a70c7026cd9605862e0
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-04-04 09:56:24 +0530

    Time: 995 ms (5.87%), Space: 145.6 MB (30.01%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        priority_queue<pair<int, int>> pq;
+        for(int i=0; i<nums.size(); i++){
+            pq.push({nums[i], i});
+            while(pq.top().second < i-k+1){
+                pq.pop();
+            }
+            // cout << nums[i] << pq.top() << " ";
+            // cout << pq.top().first;
+            cout << pq.size();
+            if(i >= k-1){
+                ans.push_back(pq.top().first); 
+            }
+        }
+        return ans;
+    }
+};
