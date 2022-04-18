dc1417c5888d664fb47e203aede36267276e178b
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-04-19 00:40:09 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (64.98%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -20,9 +20,32 @@ public:
         ans.push_back(root->val);
     }
     
+    void postOrder(TreeNode* root, vector<int> &ans){
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* temp = st1.top();
+            st1.pop();
+            st2.push(temp);
+            if(temp->left){
+                st1.push(temp->left);
+            }
+            if(temp->right){
+                st1.push(temp->right);
+            }
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+    }
+    
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        getPostOrderTree(root, ans);
+        if(!root) return ans;
+        // getPostOrderTree(root, ans);
+        postOrder(root, ans);
         return ans;
     }
 };
