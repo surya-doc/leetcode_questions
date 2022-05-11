dae7d2c8081267384ec7e98392b56091018839c8
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-05-11 09:57:16 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+	public:
+	int perfectSum(int a[], int n, int sum)
+	{
+       int tab[n + 1][sum + 1];
+  // Initializing the first value of matrix
+    tab[0][0] = 1;
+    for (int i = 1; i <= sum; i++)
+        tab[0][i] = 0;
+     
+     long long modul = 1e9 + 7;
+   
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+          // if the value is greater than the sum
+            if (a[i - 1] > j)
+                tab[i][j] = (tab[i - 1][j])%modul;
+            else
+            {
+                tab[i][j] = ((tab[i - 1][j])%modul + (tab[i - 1][j - a[i - 1]])%modul)%modul;
+            }
+        }
+    }
+ 
+ 
+    return tab[n][sum];
+	}
+	  
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+  // } Driver Code Ends
