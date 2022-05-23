add3fe3cf21e034e330c70b605dbaf4ffad5e858
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-05-23 18:15:28 +0530

    Time: 4 ms (62.99%), Space: 7.8 MB (43.14%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -19,7 +19,27 @@ public:
     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<int>> dp(obstacleGrid.size(), vector<int> (obstacleGrid[0].size(), -1));
-        return findPath(obstacleGrid, 0, 0, dp);
+        int m = size(obstacleGrid), n = size(obstacleGrid[0]);
+	    vector<vector<int> > dp (m, vector<int>(n, 0));
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        dp[0][0] = 1;
+        for(int i=1; i<m; i++){
+            dp[i][0] = (obstacleGrid[i][0] == 0 && dp[i-1][0] == 1) ? 1 : 0;
+        }
+        for(int i=1; i<n; i++){
+            dp[0][i] = (obstacleGrid[0][i] == 0 && dp[0][i-1] == 1) ? 1 : 0;
+        }
+    for(int i = 1; i < m; i++){
+        for(int j = 1; j < n; j++){
+            if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+        }
+    }
+    return dp[m-1][n-1];
     }
 };
