12a5226a17087f9ffb9bdeb0478351d9dd319486
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-05-27 22:25:03 +0530

    Time: 17 ms (8.06%), Space: 9 MB (26.07%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -16,7 +16,17 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int n = triangle[m-1].size();
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return getPathSum(triangle, 0, 0, dp);
+        vector<vector<int>> dp(m, vector<int> (n));
+        for(int i=0; i<n; i++){
+            dp[m-1][i] = triangle[m-1][i];
+        }
+        for(int i=m-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int down = triangle[i][j]+dp[i+1][j];
+                int dia = triangle[i][j]+dp[i+1][j+1];
+                dp[i][j] = min(down, dia);
+            }
+        }
+        return dp[0][0];
     }
 };
