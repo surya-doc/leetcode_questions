e9d424a5a1012cee931e766eb4a7c0fb711295e3
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-06-12 23:40:41 +0530

    Time: 807 ms (10.45%), Space: 101.7 MB (67.80%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int ans = 0;
+        int a=0;
+        map<int, int> mp;
+        int temp_sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(!mp[nums[i]]){
+                mp[nums[i]]++;
+                temp_sum += nums[i];
+            }
+            else{
+                while(mp[nums[i]] != 0){
+                    mp[nums[a]]--;
+                    temp_sum -= nums[a];
+                    a++;
+                }
+                // cout << i << "->" << temp_sum << " ";
+                mp[nums[i]]++;
+                temp_sum += nums[i];
+            }
+            ans = max(ans, temp_sum);
+        }
+        return ans;
+    }
+};
