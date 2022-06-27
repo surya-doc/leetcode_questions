c7c9fde33a67c16227d64d2d12bc59355272f38b
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-06-27 15:57:45 +0530

    Time: 213 ms (5.08%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,11 +4,8 @@ public:
         int max_val = 0;
         int i = n.size()-1;
         while(i >= 0){
-            // int temp = stoi();
-            int temp = 1;
             string a = "";
             a += n[i];
-            cout << stoi(a);
             max_val = max(max_val, stoi(a));
             i--;
         }
