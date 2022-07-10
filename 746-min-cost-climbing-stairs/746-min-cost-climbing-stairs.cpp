9c2ae4daf5b80a4ef9f9eb085a00e1b14fc6d65c
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-07-10 12:40:36 +0530

    Time: 10 ms (58.03%), Space: 13.9 MB (11.67%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int getVal(vector<int> &cost, int indx, vector<int> &dp){
+        if(indx >= cost.size()){
+            return 0;
+        }
+        if(dp[indx] != -1){
+            return dp[indx];
+        }
+        int one_step = getVal(cost, indx+1, dp);
+        int two_step = getVal(cost, indx+2, dp);
+        
+        return dp[indx] = cost[indx]+min(one_step, two_step);
+    }
+    
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1, -1);
+        return min(getVal(cost, 0, dp), getVal(cost, 1, dp));
+    }
+};
