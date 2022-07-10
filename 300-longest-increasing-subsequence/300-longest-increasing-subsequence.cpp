30bfaf209f5a3c13510a7261d6bfa3d24913aab2
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-07-10 20:38:56 +0530

    Time: 6 ms (97.85%), Space: 10.5 MB (67.29%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    
-    int getLength(vector<int> &num, int indx, int prev, vector<vector<int>> &dp){
-        if(indx >= num.size()){
-            return 0;
-        }
-        if(dp[indx][prev+1] != -1){
-            return dp[indx][prev+1];
-        }
-        int take = 0;
-        if(prev == -1 || num[indx] > num[prev]){
-            take = 1+getLength(num, indx+1, indx, dp);
-        }
-        int not_take = 0+getLength(num, indx+1, prev, dp);
-        return dp[indx][prev+1] = max(take, not_take);
-    }
-    
+    //using binary search
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> next(n+1, 0), curnt(n+1, 0);
-        for(int i=n-1; i>= 0; i--){
-            for(int j=i-1; j>=-1; j--){
-                int not_take = 0+next[j+1];
-                if(j == -1 || nums[i] > nums[j]){
-                    not_take = max(not_take, 1+next[i+1]);
-                }
-                curnt[j+1] = not_take;
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
             }
-            next = curnt;
+            else{
+                auto indx = lower_bound(temp.begin(), temp.end(), nums[i]);
+                temp[indx-temp.begin()] = nums[i];
+            }
         }
-        return next[-1+1];
+        return temp.size();
     }
 };
