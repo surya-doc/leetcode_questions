167610a73bfb2145116a71d621b974e3bb8d35ad
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-07-12 09:51:02 +0530

    Time: 621 ms (36.10%), Space: 10.1 MB (66.64%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,53 +1,21 @@
 class Solution {
 public:
-    
-//     bool getNum(vector<int> &st, int indx, int sum, int arm, vector<bool> &vis, int tot){
-//         if(tot == 0){
-//             return true;
-//         }
-//         if(sum == arm){
-//             return getNum(st, 0, 0, arm, vis, tot-1);
-//         }
-//         if(indx >= st.size()){
-//             return false;
-//         }
-//         for(int i=indx; i<st.size(); i++){
-//             if(!vis[indx]){
-//                 if(sum+st[indx] <= arm){
-//                     vis[indx] = true;
-//                     if(getNum(st, indx+1, sum+st[indx], arm, vis, tot-1)){
-//                         return true;
-//                     }
-//                     vis[indx] = false;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-    
-//     bool makesquare(vector<int>& matchsticks) {
-//         int sum = 0;
-//         for(auto it:matchsticks){
-//             sum += it;
-//         }
-//         if(sum%4 != 0) return false;
-//         sum /= 4;
-//         vector<bool> vis(16, false);
-//         return getNum(matchsticks, 0, 0, sum, vis, 4);
-//     }
     bool vis[16];
-    bool canMakeSquare(vector < int > & matchsticks, int currentSum, int oneSideLength, int totalSides, int idx = 0) {
-        if (totalSides == 0)
+    bool getNum(vector<int> &st, int curnt, int oneside, int totside, int indx=0){
+        if(totside == 0){
             return true;
-        if (currentSum == oneSideLength)
-            return canMakeSquare(matchsticks, 0, oneSideLength, totalSides - 1, 0);
-        if (idx >= size(matchsticks))
+        }
+        if(curnt == oneside){
+            return getNum(st, 0, oneside, totside-1, 0);
+        }
+        if(indx >= st.size()){
             return false;
-        for (int i = idx; i < size(matchsticks); i++) {
+        }
+        for (int i = indx; i < size(st); i++) {
             if (!vis[i]) {
-                if (currentSum + matchsticks[i] <= oneSideLength) {
+                if (curnt + st[i] <= oneside) {
                     vis[i] = true;
-                    if (canMakeSquare(matchsticks, currentSum + matchsticks[i], oneSideLength, totalSides, i + 1))
+                    if (getNum(st, curnt + st[i], oneside, totside, i + 1))
                         return true;
                     vis[i] = false;
                 }
@@ -55,8 +23,8 @@ public:
         }
         return false;
     }
-
-    bool makesquare(vector < int > & matchsticks) {
+    
+    bool makesquare(vector<int>& matchsticks) {
         memset(vis, false, sizeof(vis));
         int sum = 0;
         for (int i: matchsticks)
@@ -65,7 +33,39 @@ public:
             return false;
         int oneSideLength = sum / 4;
         int totalSides = 4;
-        return canMakeSquare(matchsticks, 0, oneSideLength, totalSides);
-
+        return getNum(matchsticks, 0, oneSideLength, totalSides);
     }
+//     bool vis[16];
+//     bool canMakeSquare(vector < int > & matchsticks, int currentSum, int oneSideLength, int totalSides, int idx = 0) {
+//         if (totalSides == 0)
+//             return true;
+//         if (currentSum == oneSideLength)
+//             return canMakeSquare(matchsticks, 0, oneSideLength, totalSides - 1, 0);
+//         if (idx >= size(matchsticks))
+//             return false;
+//         for (int i = idx; i < size(matchsticks); i++) {
+//             if (!vis[i]) {
+//                 if (currentSum + matchsticks[i] <= oneSideLength) {
+//                     vis[i] = true;
+//                     if (canMakeSquare(matchsticks, currentSum + matchsticks[i], oneSideLength, totalSides, i + 1))
+//                         return true;
+//                     vis[i] = false;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+//     bool makesquare(vector < int > & matchsticks) {
+//         memset(vis, false, sizeof(vis));
+//         int sum = 0;
+//         for (int i: matchsticks)
+//             sum += i;
+//         if (sum % 4)
+//             return false;
+//         int oneSideLength = sum / 4;
+//         int totalSides = 4;
+//         return canMakeSquare(matchsticks, 0, oneSideLength, totalSides);
+
+//     }
 };
