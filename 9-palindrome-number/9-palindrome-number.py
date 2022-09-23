77b41632231c44c89e71020217e097c341d49747
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-09-24 01:38:24 +0530

    Time: 116 ms (39.63%), Space: 13.9 MB (59.41%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -3,12 +3,8 @@ class Solution:
         a = str(a)
         i = 0
         j = len(a)-1
-        flag = 0
         for i in range(len(a)//2):
             if a[i] != a[j]:
                 return False
-                flag = 1
             j = j-1
-        if not flag:
-            return True
-        
+        return True
