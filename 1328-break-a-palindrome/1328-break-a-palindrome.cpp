f525778d940da6e42ca3e69a7a33fb4850aae25c
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-10-10 23:48:50 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (5.00%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int len = palindrome.size();
+        if(len == 1){
+            return "";
+        }
+        int flag = 0;
+        for(int i=0; i<len/2; i++){
+            if(palindrome[i] != 'a'){
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        palindrome[len-1] = 'b';
+        return palindrome;
+    }
+};
