bb30c88cb771bf0a98361e791d30ed90db68500f
Surya Dana <69008196+surya-doc@users.noreply.github.com>
2022-11-04 23:35:29 +0530

    Time: 19 ms (38.15%), Space: 7.7 MB (61.66%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
+    
+    bool mp(char a){
+        if(a == 'a' ||a == 'e' ||a == 'i' ||a == 'o' ||a == 'u' ||a == 'A' ||a == 'E' ||a == 'I' ||a == 'O' ||a == 'U'){
+            return true;
+        }
+        return false;
+    }
+    
     string reverseVowels(string s) {
-        unordered_map<char, int> mp;
-        mp['a']++;
-        mp['e']++;
-        mp['i']++;
-        mp['o']++;
-        mp['u']++;
-        mp['A']++;
-        mp['E']++;
-        mp['I']++;
-        mp['O']++;
-        mp['U']++;
+        // unordered_map<char, int> mp;
+        // mp['a']++;
+        // mp['e']++;
+        // mp['i']++;
+        // mp['o']++;
+        // mp['u']++;
+        // mp['A']++;
+        // mp['E']++;
+        // mp['I']++;
+        // mp['O']++;
+        // mp['U']++;
         int i = 0, j = s.size()-1;
         while(i < j){
-            if(mp[s[i]] && mp[s[j]]){
+            if(mp(s[i]) && mp(s[j])){
                 swap(s[i], s[j]);
                 i++;
                 j--;
             }
-            if(!mp[s[i]]){
+            if(!mp(s[i])){
                 i++;
             }
-            if(!mp[s[j]]){
+            if(!mp(s[j])){
                 j--;
             }
         }
